8e0b813f97278673cdc72da70c78adc282716e6a
Kingchen711 <Kingchenobama711@gmail.com>
2023-08-31 15:55:23 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from 'react'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { AppContext } from '../contexts/AppProvider';
+import CartModal from './CartModal';
+
+export default function Header() {
+  const [isDisplayCartModal, setIsDisplayCartModal] = useState(false);
+  const { cart } = useContext(AppContext);
+  const [totalMeals, setTotalMeals] = useState(0);
+
+  useEffect(() => {
+    if (cart?.totalMeals) {
+      setTotalMeals(cart.totalMeals)
+    }
+    else {
+      setTotalMeals(0);
+    }
+  }, [cart])
+
+  return (
+    <div className=" bg-brown-primary text-white fixed w-screen top-0 z-50">
+      {isDisplayCartModal &&
+        <div className="fixed w-screen h-screen bg-[#00000080]">
+          <CartModal setIsDisplayCartModal={setIsDisplayCartModal} />
+        </div>
+      }
+      <div className="flex justify-between items-center max-w-7xl mx-auto my-3">
+        <div className="text-3xl font-bold">ReactMeals</div>
+        <div
+          className="py-2 px-8 rounded-2xl bg-[#441403] flex justify-center items-center hover:opacity-70 cursor-pointer"
+          onClick={() => { setIsDisplayCartModal(true) }}>
+          <ShoppingCartIcon />
+          <div className="mx-3 font-bold">Your Cart</div>
+          <div className="py-1 px-4 bg-brown-primary rounded-full">{totalMeals}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
---
+++ src/contexts/AppProvider.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react"
+import { URL_BASE } from "./constant";
+import axios from "axios"
+
+
+export const AppContext = React.createContext()
+
+
+export default function AppProvider({ children }) {
+  const [cart, setCart] = useState();
+  const [meals, setMeals] = useState();
+
+
+  const [orderFormInputs, setOrderFormInputs] = useState({
+    yourName: "",
+    street: "",
+    postalCode: "",
+    city: ""
+  })
+
+
+  useEffect(() => {
+    setCart(JSON.parse(localStorage.getItem("cart")));
+  }, [])
+
+
+  useEffect(() => {
+    async function getMeals() {
+      await axios
+        .get(`${URL_BASE}/meals`)
+        .then(res => {
+          setMeals(res.data);
+        })
+    }
+    getMeals();
+  }, [])
+
+
+  function changeCountMeal(mealId, count) {
+    setCart((prev) => {
+      let updatedCart;
+      let gap;
+      let mealAmount;
+      meals.forEach((meal) => {
+        if (meal.id === mealId) {
+          mealAmount = meal.price;
+
+        }
+      })
+      if (prev && JSON.stringify(prev) !== "{}") {
+        if (prev.countMeals[mealId]) {
+          gap = count - prev.countMeals[mealId];
+        } else {
+          gap = count;
+        }
+        updatedCart = {
+          countMeals: {
+            ...prev.countMeals,
+            [mealId]: count
+          },
+          totalMeals: prev.totalMeals + gap,
+          totalAmount: prev.totalAmount + gap * mealAmount
+        }
+      }
+      else { //first cart
+        gap = count;
+        updatedCart = {
+          countMeals: {
+            [mealId]: count
+          },
+          totalMeals: gap,
+          totalAmount: gap * mealAmount
+        }
+      }
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+      return updatedCart;
+    })
+  }
+
+
+  async function sendOrder() {
+    const { yourName, street, postalCode, city } = orderFormInputs;
+    await axios.post(`${URL_BASE}/orders`, {
+      name: yourName,
+      street,
+      postalCode,
+      city,
+      cart
+    });
+  }
+
+
+  return (
+    <AppContext.Provider value={{
+      cart,
+      setCart,
+      meals,
+      changeCountMeal,
+      orderFormInputs,
+      setOrderFormInputs,
+      sendOrder
+    }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+
+
